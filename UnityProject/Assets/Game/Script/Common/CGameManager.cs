781ad85da1ed2596d0e0b4c6d26b7163f0ce89d0
Relinke <feiymx@126.com>
2018-01-04 20:07:51 +0800

    Add gamemanager

---
+++ UnityProject/Assets/Game/Script/Common/CGameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Common
+{
+    public class CGameManager : CMonoSingleton<CGameManager>
+    {
+		protected void Awake(){
+			if(CGameManager.Instance() != this){
+				Destroy(gameObject);
+				return;
+			}
+			
+		}
+    }
+}

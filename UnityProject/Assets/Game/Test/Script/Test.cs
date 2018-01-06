89c912d575fb613e4adc9e89644efb85d6e11b51
Relinke <feiymx@126.com>
2018-01-06 15:06:48 +0800

    Test for delete branch

--- UnityProject/Assets/Game/Test/Script/Test.cs
+++ UnityProject/Assets/Game/Test/Script/Test.cs
@@ -1,35 +1,42 @@
-﻿using System.Collections;
+﻿///
+// Test for game
+///
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Common;
 
-public class Test : CMonoSingleton<Test> {
-	#region Show In Inspector
-	#endregion
+public class Test : CMonoSingleton<Test>
+{
+    #region Show In Inspector
+    #endregion
 
-	#region Hide In Inspector
-	[HideInInspector]
-	public float time = 0;
-	#endregion
+    #region Hide In Inspector
+    [HideInInspector]
+    public float time = 0;
+    #endregion
 
-	#region Init Part
-	private void Awake(){
-		Init();
-	}
-	private void Init(){
+    #region Init Part
+    private void Awake()
+    {
+        Init();
+    }
+    private void Init()
+    {
 
-	}
-	#endregion
+    }
+    #endregion
 
-	#region Update Part
-	private void Update () {
-		time += Time.deltaTime;
-		
-	}
+    #region Update Part
+    private void Update()
+    {
+        time += Time.deltaTime;
 
-	private void FixedUpdate()
-	{
-		
-	}
-	#endregion
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+    #endregion
 }

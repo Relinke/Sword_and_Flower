10aaeae9f3600388ed54ad995673603060ee77c1
Relinke <feiymx@126.com>
2018-01-08 12:22:23 +0800

    delete CGameManager

---
+++ UnityProject/Assets/Game/Script/Manager/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Common.CMonoSingleton<GameManager>
+{
+    #region Show In Inspector
+	
+    #endregion
+
+    #region Hide In Inspector
+    #endregion
+
+    #region Init Part
+    protected void Awake()
+    {
+        Init();
+    }
+
+    protected void Init()
+    {
+        CheckInstance();
+    }
+
+    protected void CheckInstance()
+    {
+        if (GameManager.Instance() != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+    #endregion
+}
